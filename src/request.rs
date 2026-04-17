41312810451507ccad79e0cd6e0c111e1aefdf59
Sachin Jakhar <mesachinjakhar@gmail.com>
2026-04-17 21:06:43 +0530

    extract query paramaters

--- src/request.rs
+++ src/request.rs
@@ -4,6 +4,7 @@ use std::collections::HashMap;
 pub struct HttpRequest {
     pub method: String,
     pub path: String,
+    pub query: HashMap<String, String>,
     pub version: String,
     pub headers: HashMap<String, String>,
     pub body: Option<String>,
@@ -16,8 +17,11 @@ pub fn parse(header: String, body: String) -> HttpRequest {
     let mut parts = request_line.split_whitespace();
 
     let method = parts.next().unwrap_or("").to_string();
-    let path = parts.next().unwrap_or("").to_string();
+    let raw_path = parts.next().unwrap_or("").to_string();
     let version = parts.next().unwrap_or("").to_string();
+
+    let (path, query) = parse_path(&raw_path);
+
 
     // parse remaining lines key : value; eg: Host: localhost:8080 , User-Agent: insomnia/12.4.0
 
@@ -38,8 +42,30 @@ pub fn parse(header: String, body: String) -> HttpRequest {
     HttpRequest {
         method,
         path,
+        query,
         version,
         headers,
         body,
+    }
+}
+
+fn parse_path(raw_path: &str) -> (String, HashMap<String, String>) {
+    let mut query = HashMap::new();
+
+    // split at '?' → ["/search", "q=rust&page=2"]
+    match raw_path.split_once('?') {
+        None => {
+            // no query string at all
+            (raw_path.to_string(), query)
+        }
+        Some((path, query_string)) => {
+            // "q=rust&page=2" → ["q=rust", "page=2"]
+            for pair in query_string.split('&') {
+                if let Some((key, value)) = pair.split_once('=') {
+                    query.insert(key.to_string(), value.to_string());
+                }
+            }
+            (path.to_string(), query)
+        }
     }
 }
